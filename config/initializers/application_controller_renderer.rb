253bc9bada8ef135546ca63cde4f0853e8d49bbc
akadashiso <akadashiso@gmail.com>
2021-01-21 07:41:21 +0000

    [Finish]Meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ecc1159cc9766c9790076f9fd1375cc013630258e8312192d1f5766ad9fbec76a7650c3481bca16843dde7df4605e584fe21c758ba0e35b1f71ee1d0110a482f'
